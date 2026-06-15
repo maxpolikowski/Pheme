a2e7a30905a351e47a75ba775f66889280c9c933
Max <max.polikowski@gmail.com>
2026-06-15 20:50:13 +0200

    final version

--- pheme/logowanie-app/server.js
+++ pheme/logowanie-app/server.js
@@ -35,7 +35,7 @@ async function sendBrevoEmail(toEmail, subject, textContent, htmlContent) {
             },
             body: JSON.stringify({
                 // 🔥 WAŻNE: Wpisz tutaj adres e-mail, którym logujesz się do Brevo!
-                sender: { name: "Pheme App", email: "TWÓJ_EMAIL_Z_BREVO@gmail.com" }, 
+                sender: { name: "Pheme App", email: "pheme.panel.studenta@gmail.com" }, 
                 to: [{ email: toEmail }],
                 subject: subject,
                 textContent: textContent,
